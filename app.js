d780497a88f1f7e6a668bb273a23a0db5c814a9f
Rofi Akmaluddin <r.akmaluddin@gmail.com>
2021-02-13 22:39:17 +0700

    prepare to deploy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
 }
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
